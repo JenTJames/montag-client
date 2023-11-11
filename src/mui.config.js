35245ca2bc511e1dbc39cf9f6d0c50e151498022
Jen Thomas James <jenthomasjames@gmail.com>
2023-11-11 10:10:49 +0530

    Config: Add Light theme colors and override Textfield of MUI

--- src/mui.config.js
+++ src/mui.config.js
@@ -1,5 +1,37 @@
 import { createTheme } from "@mui/material/styles";
 
-export const lightTheme = createTheme();
+const LIGHTCOLORS = {
+  primary: "#7635DC",
+  primaryContrast: "#FFF",
+  secondary: "#222831",
+  secondaryContrast: "#EEF1F6",
+  subtle: "#6C737F",
+  highlight: "#f3f4f6",
+};
+
+export const lightTheme = createTheme({
+  palette: {
+    primary: {
+      main: LIGHTCOLORS.primary,
+      contrastText: LIGHTCOLORS.primaryContrast,
+    },
+    secondary: {
+      main: LIGHTCOLORS.secondary,
+      contrastText: LIGHTCOLORS.secondaryContrast,
+    },
+    subtle: LIGHTCOLORS.subtle,
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "&:hover": {
+            backgroundColor: LIGHTCOLORS.highlight,
+          },
+        },
+      },
+    },
+  },
+});
 
 export const darkTheme = createTheme();
