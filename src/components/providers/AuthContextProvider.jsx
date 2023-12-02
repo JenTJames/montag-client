b434be8db470be2eeefc40c12247ce103de6f87d
Jen Thomas James <jenthomasjames@gmail.com>
2023-12-02 17:37:01 +0530

    Feat: Add authentication context

---
+++ src/components/providers/AuthContextProvider.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import AuthContext from "../../store/auth-context";
+
+const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    id: 0,
+    firstname: "",
+    lastname: "",
+    email: "",
+  });
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthContextProvider;
