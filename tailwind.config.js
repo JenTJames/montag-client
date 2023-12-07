f1f0c3bde201b3033df03ecb3985077b36b52516
Jen Thomas James <jenthomasjames@gmail.com>
2023-12-07 18:14:15 +0530

    Config: Add color palette to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#f5f3ff",
+          100: "#ede9fe",
+          200: "#ddd6fe",
+          300: "#c4b5fd",
+          400: "#a78bfa",
+          500: "#8b5bf7",
+          600: "#7c39ee",
+          700: "#7635dc",
+          800: "#5b20b7",
+          900: "#4c1d95",
+          950: "#2e1065",
+        },
+        secondary: {
+          50: "#f6f7f9",
+          100: "#eceff2",
+          200: "#d4dbe3",
+          300: "#aebccb",
+          400: "#8297ae",
+          500: "#637b94",
+          600: "#4e637b",
+          700: "#405064",
+          800: "#384454",
+          900: "#323c48",
+          950: "#222831",
+        },
+        secondaryContrast: "#EEF1F6",
+        subtle: "#6C737F",
+      },
+    },
   },
   plugins: [],
 };
