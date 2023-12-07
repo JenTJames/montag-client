dd6cbfda6d0b883bae9c72246c3c79556774a37c
Jen Thomas James <jenthomasjames@gmail.com>
2023-12-07 16:21:24 +0530

    Comp: Add Sidebar and Navlink components

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,55 @@
+import { styled } from "styled-components";
+import PropTypes from "prop-types";
+import { NavLink as MuiNavLink } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: ${({ orientation }) =>
+    orientation === "vertical" ? "column" : "row"};
+  justify-content: ${({ orientation }) =>
+    orientation === "vertical" ? "center" : "space-between"};
+  align-items: center;
+  gap: ${({ orientation }) => (orientation === "vertical" ? "5px" : "25px")};
+`;
+
+const StyledTextWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: ${({ orientation }) =>
+    orientation === "vertical" ? "center" : "flex-start"};
+`;
+
+const NavLink = ({ to, label, icon, orientation = "vertical" }) => {
+  return (
+    <MuiNavLink
+      className={(navData) =>
+        navData.isActive
+          ? "p-3 bg-purple-100 rounded-md text-primary"
+          : "p-3 hover:bg-purple-50 rounded-md text-subtle"
+      }
+      to={to}
+    >
+      <StyledDiv orientation={orientation}>
+        {icon}
+        <StyledTextWrapper orientation={orientation}>
+          <Typography
+            className="text-center text-primaryt-500"
+            variant={orientation === "vertical" ? "caption" : "subtitle1"}
+          >
+            {label}
+          </Typography>
+        </StyledTextWrapper>
+      </StyledDiv>
+    </MuiNavLink>
+  );
+};
+
+NavLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
+  orientation: PropTypes.string,
+};
+
+export default NavLink;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { MdDashboard } from "react-icons/md";
+import { CgChevronLeft, CgChevronRight } from "react-icons/cg";
+
+import Logo from "../assets/brand.svg";
+import useToggle from "../hooks/use-toggle";
+
+import NavLink from "./NavLink";
+
+const StyledSideBar = styled.div`
+  width: ${({ $isExpanded }) => ($isExpanded ? "15rem" : "8rem")};
+`;
+
+const Sidebar = () => {
+  const { value: isExpanded, toggle: toggleExpansion } = useToggle();
+  return (
+    <div className="relative">
+      <StyledSideBar
+        $isExpanded={isExpanded}
+        className="min-h-screen flex-0 bg-transparent border-r border-dashed border-r-slate-200 py-5 px-2 flex flex-col gap-7 items-center"
+      >
+        <img className="w-12" src={Logo} alt="brand" />
+        <div className="flex flex-col gap-3">
+          <NavLink
+            label="Dashboard"
+            icon={<MdDashboard size={25} />}
+            to="/dashboard"
+            orientation={isExpanded ? "horizontal" : "vertical"}
+          />
+        </div>
+      </StyledSideBar>
+      <div
+        onClick={() => {
+          toggleExpansion();
+        }}
+        className="absolute p-1 top-7 -right-3 bg-white border border-slate-200 border-dashed rounded-full"
+      >
+        {isExpanded ? <CgChevronLeft /> : <CgChevronRight />}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
