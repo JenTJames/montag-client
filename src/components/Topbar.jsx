9d80e6219157a40e4c9c77460a8f7965b01c76d4
Jen Thomas James <jenthomasjames@gmail.com>
2023-12-09 14:33:09 +0530

    Feat: Add dropdown for settings in topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,29 +1,65 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
+import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import { IoNotifications } from "react-icons/io5";
+import Typography from "@mui/material/Typography";
+import { IoNotifications, IoSettings } from "react-icons/io5";
 
-const Topbar = () => {
+import AuthContext from "../store/auth-context";
+
+import Menu from "./Menu";
+import MenuItem from "./MenuItem";
+
+const TopBar = () => {
   const navigate = useNavigate();
 
+  const { setUser } = useContext(AuthContext);
+
   const logoutHandler = () => {
+    setUser({
+      id: 0,
+      firstname: "",
+      lastname: "",
+      email: "",
+    });
     localStorage.clear();
-    navigate("/login");
+    navigate("/login", {
+      replace: true,
+    });
   };
 
   return (
-    <div className="flex gap-4 justify-end w-full p-6">
-      <Tooltip title="Notifications">
-        <IconButton>
-          <IoNotifications />
-        </IconButton>
-      </Tooltip>
-      <Button color="secondary" variant="contained" onClick={logoutHandler}>
-        Logout
-      </Button>
+    <div className="w-full flex justify-end sticky top-0 p-6 gap-3">
+      <IconButton>
+        <IoNotifications />
+      </IconButton>
+      <Menu
+        component={
+          <IconButton>
+            <IoSettings />
+          </IconButton>
+        }
+      >
+        <div className="flex flex-col px-4 my-2 cursor-default">
+          <h1 className="font-semibold text-secondary-700">Jen James</h1>
+          <Typography className="text-secondary-500" variant="subtitle2">
+            jenthomasjames@gmail.com
+          </Typography>
+        </div>
+        <Divider />
+        <div className="flex flex-col my-3">
+          <MenuItem onClick={() => {}}>Profile</MenuItem>
+          <MenuItem onClick={() => {}}>Settings</MenuItem>
+        </div>
+        <Divider />
+        <div className="my-3">
+          <MenuItem onClick={logoutHandler} color="danger">
+            Logout
+          </MenuItem>
+        </div>
+      </Menu>
     </div>
   );
 };
 
-export default Topbar;
+export default TopBar;
