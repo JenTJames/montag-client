072fea9bdd7190fabe0ebc9673396701ef062ef0
Jen Thomas James <jenthomasjames@gmail.com>
2024-01-13 18:20:53 +0530

    Util: Add global utility file for storing hardcoded values

---
+++ src/utils/global.js
@@ -0,0 +1,41 @@
+export const EMPLOYMENT_TYPES = [
+  {
+    value: "fullTime",
+    label: "Full Time",
+  },
+  {
+    value: "partTime",
+    label: "Part Time",
+  },
+  {
+    value: "onDemand",
+    label: "On Demand",
+  },
+  {
+    value: "contract",
+    label: "Contract",
+  },
+];
+
+export const EXPERIENCE_LEVELS = [
+  {
+    value: "none",
+    label: "No Experience",
+  },
+  {
+    value: "one",
+    label: "1 Year",
+  },
+  {
+    value: "two",
+    label: "2 Years",
+  },
+  {
+    value: "three",
+    label: "3 Years",
+  },
+  {
+    value: "moreThanFour",
+    label: "4+ Years",
+  },
+];
